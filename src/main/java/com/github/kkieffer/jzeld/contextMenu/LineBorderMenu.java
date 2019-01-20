0cf61eab0b207485685ddaae19ae73f71a68cc64
kkieffer <kkieffer@c2633-mbp.lan>
2019-01-19 20:37:55 -0500

    Adding new menu item for border thickness that brings up a popup to set a user-entered value.

--- src/main/java/com/github/kkieffer/jzeld/contextMenu/LineBorderMenu.java
+++ src/main/java/com/github/kkieffer/jzeld/contextMenu/LineBorderMenu.java
@@ -2,15 +2,18 @@
 package com.github.kkieffer.jzeld.contextMenu;
 
 import com.github.kkieffer.jzeld.ZCanvas;
+import static com.github.kkieffer.jzeld.ZCanvas.errorIcon;
 import java.awt.BasicStroke;
 import static java.awt.BasicStroke.CAP_SQUARE;
 import static java.awt.BasicStroke.JOIN_MITER;
+import java.awt.ComponentOrientation;
 import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsEnvironment;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import javax.swing.ImageIcon;
 import javax.swing.JMenu;
+import javax.swing.JOptionPane;
 
 /**
  * LineBorderMenu class provides a JMenu with two sub menu items for line weight and line dash pattern.  When an menu item is selected,
@@ -22,9 +25,10 @@ public class LineBorderMenu extends JMenu {
 
    public enum Type {WEIGHT, DASH}
     
+    public static final ImageIcon lineIcon = new ImageIcon(ZCanvas.class.getResource("/line.png")); 
+
     
-    
-    private static final float[] WIDTHS = {0.0f, 0.25f, 0.5f, 1.0f, 1.5f, 2.0f, 3.0f, 4.0f, 6.0f, 8.0f};
+    private static final float[] WIDTHS = {0.0f, 0.25f, 0.5f, 1.0f, 1.5f, 2.0f, 3.0f, 4.0f, 6.0f, 8.0f, -1};  //-1 is custom
     private static final Float[][] DASHES = {new Float[0],
                                         new Float[]{.025f, .025f},
                                         new Float[]{.025f, .05f},
@@ -49,25 +53,43 @@ public class LineBorderMenu extends JMenu {
 
             DecimalFormat fmt = new DecimalFormat("0.0");
             
-            g.setStroke(new BasicStroke(thickStep));
-            String label = fmt.format(thickStep);
-            setText(label);
-            
-            if (thickStep > 0.0)
-                g.drawLine(10, 8, 90, 8);
+            if (thickStep >= 0) {
+                g.setStroke(new BasicStroke(thickStep));
+                String label = fmt.format(thickStep);
+                setText(label);            
+                g.drawLine(10, 8, 90, 8);                    
+            }
+            else 
+                setText("Set..");
             
             g.dispose();
-            
+
             this.setIcon(new ImageIcon(bufferedImage));
-                        
-            
+        
             addActionListener(new java.awt.event.ActionListener() {
                 @Override
                 public void actionPerformed(java.awt.event.ActionEvent evt) {
                     if (canvas == null)
                         return;
-                    canvas.setOutlineWidth(thickStep);
-                    lineWidthChanged(thickStep);
+                    
+                    if (thickStep < 0) {
+                        //Prompt for custom size
+                        String rc = (String)JOptionPane.showInputDialog(canvas.getParent(), "Set Custom Line Width", "Modify Line Width", JOptionPane.QUESTION_MESSAGE, lineIcon, (Object[])null, "");
+                        if (rc != null) {
+                            try {
+                                float f = Float.parseFloat(rc);
+                                if (f < 0)
+                                    throw new NumberFormatException("Value cannot be negative");
+                                 canvas.setOutlineWidth(f); 
+                            } catch (NumberFormatException ex) {
+                                JOptionPane.showMessageDialog(canvas.getParent(), "Invalid value: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE, errorIcon);
+                            }
+                        }
+                    }
+                    else {
+                        canvas.setOutlineWidth(thickStep);
+                        lineWidthChanged(thickStep);
+                    }
                 }
             });
         }
@@ -131,6 +153,7 @@ public class LineBorderMenu extends JMenu {
             case WEIGHT: //Solid lines
                 for (float i : WIDTHS) {
                     WeightMenuItem weightMenuItem = new WeightMenuItem(c, gC, i);
+                    weightMenuItem.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
                     menuItemList.add(weightMenuItem);
                     this.add(weightMenuItem);   
                 }
